efb00285b4c4cc3a862f54514ff00eb9066cacb2
Yusuf Kerim TÜYSÜZ <yusufkerimtuysuz@gmail.com>
2021-07-19 23:14:12 +0300

    ConsoleUI project added

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sellamaleykkuum");
+        }
+    }
+
+}
